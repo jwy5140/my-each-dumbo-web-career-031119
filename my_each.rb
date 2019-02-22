5a80f03f2df304696090611505b0363d813efe08
Jeffrey Yu <jwy5140@gmail.com>
2019-02-22 04:01:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,11 @@
 def my_each(arr)
-  n = 0 
-  while n < arr.size do
-    yield(arr[n])
-    n += 1 
-  end
-  arr
+  
+    n = 0 
+    while n < arr.size do
+      yield(arr[n])
+      n += 1 
+    end
+    arr
 end
 
 
